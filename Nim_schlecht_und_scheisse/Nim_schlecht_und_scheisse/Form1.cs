be4e3ebe488ed53575653be0b5dd2d420ee4b460
Cyberworm3285 <cyberworm3285@web.de>
2016-09-01 23:44:52 +0200

    Add files via upload

---
+++ Nim_schlecht_und_scheisse/Nim_schlecht_und_scheisse/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Nim_schlecht_und_scheisse
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            add(1);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            add(2);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            add(3);
+        }
+
+        private void add(int a)
+        {
+            label2.Text = "";
+            if (label1.Text.Length + a >= 11) loose();
+            else label1.Text += new string('O',a);
+        }
+
+        private void loose()
+        {
+            label1.Text = "";
+            label2.Text = "du hast verloren";
+        }
+
+    }
+}
